7adf8bbaf28fcf4dfc06494a36ca3add7594992b
nathanielsean88-afk <nathanielsean88@gmail.com>
2026-05-31 06:23:32 +0700

    Orange comic neobrutalism UI

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'smp-pgri-v1';
+const ASSETS = ['/', '/css/style.css', '/js/main.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
